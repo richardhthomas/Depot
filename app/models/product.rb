e0c01a0b0f691f7f2b8e5b1469106ea755aa8174
richardhthomas <richardhthomas@yahoo.com>
2013-06-25 16:57:13 +0100

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,14 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  validates :title, :description, :image_url, :presence => true
+  validates :price, :numericality => {
+    :greater_than_or_equal_to => 0.01, 
+    :message => 'Nonsense, Must be worth something.'
+    }
+  validates :title, :uniqueness => true
+  validates :title, :length => {:minimum => 10}
+  validates :image_url, :format => {
+    :with => %r{\.(gif|jpg|png)$}i,
+    :message => 'must be a URL for GIF, JPG or PNG image.'
+    }
 end
