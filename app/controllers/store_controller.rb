6d31ec83d2455ee8faba2708065dc85493e9d66e
richardhthomas <richardhthomas@yahoo.com>
2013-07-16 17:44:58 +0100

    Chapter 11 playtime v1

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,7 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    @cart = current_cart
     # Record how many times the user has accessed the store's index page
     if session[:counter].nil?
       session[:counter] = 1
